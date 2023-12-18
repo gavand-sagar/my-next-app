eb895eee3679dc1e45331b06585b2b1f682e583a
gavand-sagar <gavand.sagar.s@gmail.com>
2023-12-18 17:32:01 +0530

    temp

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -50,11 +50,10 @@ export default function Page() {
                     columns={columns}
                     initialState={{
                         pagination: {
-                            paginationModel: { page: 0, pageSize: 5 },
+                            paginationModel: { page: 0, pageSize: 10 },
                         },
                     }}
                     pageSizeOptions={[5, 10]}
-                    checkboxSelection
                 />
             }
         </div>
