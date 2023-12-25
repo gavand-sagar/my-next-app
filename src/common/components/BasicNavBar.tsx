ff7ed5d2b38d4a5566acc51807f409157ed6895e
gavand-sagar <gavand.sagar.s@gmail.com>
2023-12-25 17:18:29 +0530

    complet fix

--- src/common/components/BasicNavBar.tsx
+++ src/common/components/BasicNavBar.tsx
@@ -1,11 +1,13 @@
 import Link from 'next/link'
 import React from 'react'
 import BasicNav from './BasicNav.module.css'
-import { Paper } from '@mui/material'
+import { Button, Paper } from '@mui/material'
 export default function BasicNavBar() {
   return (
     <Paper className={BasicNav.myNav}>
-        <Link href={'/home'}>Home</Link>
+        <Link href={'/home'}>
+          <Button>Home</Button>
+        </Link>
         <Link href={'/products'}>Products</Link>
         <Link href={'/login'}>Login</Link>
     </Paper>
